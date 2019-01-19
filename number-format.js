9da651244f42fb23bb76165d0fd0719ea7761af5
Tobias Armbruster <tobias.armbruster@omikron.net>
2019-01-19 21:21:00 +0100

    initial

---
+++ number-format.js
@@ -0,0 +1,61 @@
+class NumberFormat extends HTMLElement {
+    static get is() {
+        return "number-format";
+    }
+
+    static get properties() {
+        return {
+            number: String,
+            minDigits: {
+                attribute: "min-digits"
+            },
+            maxDigits: {
+                attribute: "max-digits"
+            },
+            currency: String,
+            locale: String
+        };
+    }
+
+    static get observedAttributes() {
+        return ["number", "minDigits", "maxDigits", "currency", "locale"];
+    }
+
+    constructor() {
+        super();
+        this.minDigits = 2;
+        this.maxDigits = 2;
+        this.locale = navigator.language;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this[name] = newValue;
+        this.render();
+    }
+
+    render() {
+        requestAnimationFrame(() => {
+            if (this.number) {
+                this.innerText = new Number(this.number).toLocaleString(this.locale, this.getLocaleProperties())
+            } else {
+                this.innerText = "";
+            }
+        });
+    }
+
+    getLocaleProperties() {
+        const {minDigits, maxDigits, currency} = this;
+        return {
+            minimumFractionDigits: minDigits,
+            maximumFractionDigits: maxDigits,
+            style: this.getStyle(),
+            currency
+        };
+    }
+
+    getStyle() {
+        return this.currency !== void 0 ? "currency" : "decimal";
+    }
+}
+
+customElements.define(NumberFormat.is, NumberFormat);
